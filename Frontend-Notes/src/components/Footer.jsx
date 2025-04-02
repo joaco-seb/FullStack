59e0f47b595cb9779fb0b9169aa639e0ce060823
JoaquinHubner <joaquinhubner42@gmail.com>
2025-04-02 17:28:16 -0300

    First Commit

---
+++ Frontend-Notes/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {  
+
+    return (    
+        <div className="footer">     
+            <br />      
+            <em>Note app, Department of Computer Science, University of Helsinki 2024</em>    
+        </div>  
+    )
+}
+
+export default Footer
